2c182e718700edcbadfcce9af38fe72e16088ede
Alfred Needham <17needa@olab.org.uk>
2023-02-23 18:59:06 +0000

    huffmann tree almost working

---
+++ 2023/huffman_tree_compressor.py
@@ -0,0 +1,50 @@
+text = str(input("Enter text to compress here: "))
+
+dictOfCharacters = {}
+dictOfHuffmanCodes = {}
+huffmanCode = ""
+
+# * Function that returns the smallest occurring character and its number of occurrences, after deleting that character from the dictionary.
+def find_smallest_occurrence():
+    minimumValue = (min(dictOfCharacters.values()))
+    for char in dictOfCharacters:
+        if dictOfCharacters[char] == minimumValue:
+            smallestChar = char
+            value = dictOfCharacters[char]
+            dictOfCharacters.pop(char)
+            return(smallestChar, value)
+
+# * Each character in text become a key. The number of occurences is the value.
+for char in range(len(text)):
+    if text[char] in dictOfCharacters:
+        dictOfCharacters[text[char]] += 1
+    else:
+        dictOfCharacters[text[char]] = 1
+
+# * Adds the characters to the huffman dictionary, assigning them a value of 0 or 1. The final combination is given a value of None.
+while len(dictOfCharacters) > 1:   
+    value1 = 0
+    value2 = 0
+    smallestChar1 = ""
+    smallestChar2 = ""
+    smallestChar1, value1 = ((find_smallest_occurrence()))
+    smallestChar2, value2 = ((find_smallest_occurrence()))    
+    dictOfHuffmanCodes[smallestChar1] = (smallestChar1 + smallestChar2, 0)
+    dictOfHuffmanCodes[smallestChar2] = (smallestChar1 + smallestChar2, 1)
+    dictOfCharacters[smallestChar1 + smallestChar2] = value1 + value2   
+for char in dictOfCharacters:
+    dictOfHuffmanCodes[char] = None
+   
+   
+    
+def create_huffman_code(char):
+    print(char)
+    if dictOfHuffmanCodes[char][0] is None:
+        return()
+    else:
+        return(dictOfHuffmanCodes[create_huffman_code[char][0]] + create_huffman_code[char][1])
+    
+for i in range(len(text)):
+    code = create_huffman_code(text[i])
+
+print(huffmanCode)
