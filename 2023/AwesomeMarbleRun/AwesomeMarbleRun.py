9f0309745a370090347217fa0fae720446e639da
Alfred Needham <17needa@olab.org.uk>
2023-03-14 16:00:13 +0000

    more progress on marble run

--- 2023/AwesomeMarbleRun/AwesomeMarbleRun.py
+++ 2023/AwesomeMarbleRun/AwesomeMarbleRun.py
@@ -8,14 +8,13 @@ from math import sin, cos
 
 class Marble(Widget):
     canCollide = True
-
     velocity_x = NumericProperty(0)
     velocity_y = NumericProperty(0)
     velocity = ReferenceListProperty(velocity_x, velocity_y)
 
     def gravity(self):
         terminalVelocity = 15
-        accelerationDueToGravity = 0.5 / 4
+        accelerationDueToGravity = 0.5
         if self.velocity_y > -terminalVelocity:
             self.velocity_y -= accelerationDueToGravity
         
@@ -23,9 +22,12 @@ class Marble(Widget):
         self.pos = Vector(*self.velocity) + self.pos
 
     def detect_collision(self, platform):
+        if self.collide_widget(platform):
+            return(True)
+
+    def bounce_marble(self, platform):
         elasticity = 0.8
         if self.collide_widget(platform) and Marble.canCollide == True:
-            print("Collided!", platform.angle)
             if platform.angle == 0 or platform.angle == 180:
                 self.velocity_y *= -elasticity
             if platform.angle == 90 or platform.angle == 270:
@@ -35,23 +37,15 @@ class Marble(Widget):
                 self.velocity_y = -elasticity * self.velocity_x
                 self.velocity_x = -elasticity * tempVelocity
             if platform.angle == 135 or platform.angle == 315:
-                pass
-
-
-
-            # * equation time l;ets govequaitons go ehe por fasvpfr
-
-
-
+                tempVelocity = self.velocity_y
+                self.velocity_y = -elasticity * self.velocity_x
+                self.velocity_x = elasticity * tempVelocity
             Marble.canCollide = False
             Clock.schedule_once(Marble.unlock_collision, 0.01)
-            return(True)
-        else:
-            return(False)
+            platform.parent.y += 1
         
     def unlock_collision(dt):
         Marble.canCollide = True
-        print("Unlocked")
 
 class RectanglePlatform(Widget):
     topline = ObjectProperty(None)
@@ -68,21 +62,22 @@ class RightLine(Widget):
     pass
 
 class Scene(Widget):
-    marble = ObjectProperty(None)
-    platform = ObjectProperty(None)
 
     def update_scene(self, dt):
-        for child in self.platform.children:
-            if self.marble.detect_collision(child) == False:
-                self.marble.gravity()
+        for i in self.platforms:
+            if self.marble.detect_collision(i) == True:
+                for child in i.children:
+                    self.marble.bounce_marble(child)
+        self.marble.gravity()
         self.marble.move()
 
 class awesomeMarbleRunApp(App):
     def build(self):
+        Window.clearcolor=(0.2,0.6,0.2,1)
         scene = Scene()
         Clock.schedule_interval(scene.update_scene, 1.0 / 120.0)
         return scene
-        
+      
 if __name__ == "__main__":
     Window.size = (500, 750)
     awesomeMarbleRunApp().run()
