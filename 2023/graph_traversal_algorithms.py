5b4b68c52479b5bf3ca2c93b640542f542b94ff4
Alfred Needham <17needa@olab.org.uk>
2023-05-12 14:09:10 +0100

    depth first edits

--- 2023/graph_traversal_algorithms.py
+++ 2023/graph_traversal_algorithms.py
@@ -1,16 +1,10 @@
 time = 0
 
-class Graph():
-
-    def __init__(self, vertices):
-        self.list = []
-
-    def input_vertex(self, vertex):
-        self.list.append(vertex)
-
-def input_edge(vertexOne, vertexTwo):
+def input_edge(vertexOne, vertexTwo, type):
+    # type == 1: non-directed graph. type == 2: directed graph.
     vertexOne.neighbours.append(vertexTwo)
-    vertexTwo.neighbours.append(vertexOne)
+    if type == 1:
+        vertexTwo.neighbours.append(vertexOne)
 
 class Vertex():
     def __init__(self, name):
@@ -20,24 +14,22 @@ class Vertex():
         self.parent = None
         self.neighbours = []
 
-def depth_visit(vertex):
+def __depth_visit(vertex):
     global time
     time += 1
-    print(vertex.name)
     vertex.entryTime = time
     for neighbour in vertex.neighbours:
         if neighbour.entryTime == None:
             neighbour.parent = vertex
-            depth_visit(neighbour)
-        time += 1
-        vertex.exitTime = time
+            __depth_visit(neighbour)
+    time += 1
+    vertex.exitTime = time
 
 def depth_first_traversal(graph):
-    for vertex in graph.list:
+    for vertex in graph:
         if vertex.entryTime == None:
-            depth_visit(vertex)
+            __depth_visit(vertex)
 
-p = Graph(7)
 a = Vertex("a")
 b = Vertex("b")
 c = Vertex("c")
@@ -51,36 +43,34 @@ j = Vertex("j")
 k = Vertex("k")
 l = Vertex("l")
 
-p.input_vertex(a)
-p.input_vertex(b)
-p.input_vertex(c)
-p.input_vertex(d)
-p.input_vertex(e)
-p.input_vertex(f)
-p.input_vertex(g)
-p.input_vertex(h)
-p.input_vertex(i)
-p.input_vertex(j)
-p.input_vertex(k)
-p.input_vertex(l)
+p = [a,b,c,d,e,f,g,h,i,j,k,l]
 
-input_edge(a, b)
-input_edge(e, b)
-input_edge(e, f)
-input_edge(a, c)
-input_edge(c, d)
-input_edge(d, f)
-input_edge(d, g)
-input_edge(g, h)
-input_edge(g, i)
-input_edge(i, j)
-input_edge(i, k)
-input_edge(i, l)
+input_edge(a, b,1)
+input_edge(e, b,1)
+input_edge(e, f,1)
+input_edge(a, c,1)
+input_edge(c, d,1)
+input_edge(d, f,1)
+input_edge(d, g,1)
+input_edge(g, h,1)
+input_edge(g, i,1)
+input_edge(i, j,1)
+input_edge(i, k,1)
+input_edge(i, l,1)
+input_edge(k, l,1)
 
+def sort_neighbours(vertex):
+    for passes in range(len(vertex.neighbours)):
+        madeSwap = False
+        for comparisons in range(len(vertex.neighbours)-passes-1):
+            if vertex.neighbours[comparisons].name > vertex.neighbours[comparisons+1].name:
+                madeSwap = True
+                vertex.neighbours[comparisons].name, vertex.neighbours[comparisons+1].name = vertex.neighbours[comparisons+1].name, vertex.neighbours[comparisons].name
+        if madeSwap == False:
+            break
 
-def print_neighbours(vertex):
-    for neighbour in vertex.neighbours:
-        print(neighbour.name)
+for vertex in p:
+    sort_neighbours(vertex)
 
-depth_visit(a)
+depth_first_traversal(p)
 print(a.entryTime, a.exitTime)
