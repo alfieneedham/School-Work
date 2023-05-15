ed3826013cae4ba6bb5c1d9bbb3344e2f1d4a908
Alfred Needham <17needa@olab.org.uk>
2023-05-15 17:39:24 +0100

    Dijkstras working

--- 2023/dijkstras_algorithm.py
+++ 2023/dijkstras_algorithm.py
@@ -13,35 +13,52 @@ class Vertex():
         self.neighbours = []
 
 def dijkstras_algorithm(graph, start, destination):
-
+    path = []
     def find_index(vertex):
         for i in range(len(vertices)):
             if vertices[i][0].name == vertex.name:
                 return(i)
-
+            
     queue = NaivePriorityQueue()
     vertices = []
     for vertex in graph:
         # tuple: (vertex, parent, distance to vertex)
         vertices.append([vertex, None, inf])
-
+        
     vertices[find_index(start)][1] = start
     vertices[find_index(start)][2] = 0
     queue.enqueue(start, 0)
-
-    while queue.is_empty():
-        currentVertex = queue.dequeue
-        for neighbour in currentVertex.neighbours:
+    
+    while queue.is_empty() == False:
+        currentVertex = queue.dequeue()
+        for neighbour in currentVertex[0].neighbours:
             if vertices[find_index(currentVertex[0])][2] + neighbour[1] < vertices[find_index(neighbour[0])][2]:
                 vertices[find_index(neighbour[0])][2] = vertices[find_index(currentVertex[0])][2] + neighbour[1]
-                vertices[find_index(neighbour[0])][1] = currentVertex
-                queue.enqueue(neighbour[0], vertices[find_index(currentVertex[0])][2] + neighbour[1])
-
-    for vertex in vertices:
+                vertices[find_index(neighbour[0])][1] = currentVertex[0]
+                queue.enqueue(neighbour[0], vertices[find_index(currentVertex[0])][2] + neighbour[1])  
+                   
+    currentVertex = vertices[find_index(destination)]
+    distance = currentVertex[2]
+    while currentVertex[0] != start:
         try:
-            print(vertex[0].name, vertex[1].name, vertex[2])
+            path.append(currentVertex[0])
+            currentVertex = vertices[find_index(currentVertex[1])]
         except:
-            print(vertex[0].name, vertex[1], vertex[2])
+            print("No valid path.")
+            quit()
+            
+    path.append(start)
+    reversedPath = reversed(path)
+    for vertex in reversedPath:
+        print(vertex.name)
+    print(distance)
+    return(reversedPath, distance)
+
+    #for vertex in vertices:
+        #try:
+        #print(vertex[0].name, vertex[1].name, vertex[2])
+        #except:
+        #    print(vertex[0].name, vertex[1], vertex[2])
     
 
 
@@ -60,4 +77,4 @@ input_edge(b, c, 120)
 input_edge(c, e, 100)
 input_edge(b, e, 200)
 
-dijkstras_algorithm(p, a, b)
+dijkstras_algorithm(p, a, c)
--- 2023/graph_traversal_algorithms.py
+++ 2023/graph_traversal_algorithms.py
@@ -1,9 +1,12 @@
+from abstract_data_types import CircularQueue, NaivePriorityQueue
+from math import inf
+
 time = 0
 
 def input_edge(vertexOne, vertexTwo, type):
-    # type == 1: non-directed graph. type == 2: directed graph.
+    # type == 1: directional graph. type == 2: non-directional graph.
     vertexOne.neighbours.append(vertexTwo)
-    if type == 1:
+    if type == 2:
         vertexTwo.neighbours.append(vertexOne)
 
 class Vertex():
@@ -14,63 +17,97 @@ class Vertex():
         self.parent = None
         self.neighbours = []
 
-def __depth_visit(vertex):
+def depth_visit(vertex):
     global time
     time += 1
+    print(vertex.name)
     vertex.entryTime = time
     for neighbour in vertex.neighbours:
         if neighbour.entryTime == None:
             neighbour.parent = vertex
-            __depth_visit(neighbour)
+            depth_visit(neighbour)
     time += 1
     vertex.exitTime = time
 
 def depth_first_traversal(graph):
     for vertex in graph:
         if vertex.entryTime == None:
-            __depth_visit(vertex)
+            depth_visit(vertex)
 
-a = Vertex("a")
-b = Vertex("b")
-c = Vertex("c")
-d = Vertex("d")
-e = Vertex("e")
-f = Vertex("f")
-g = Vertex("g")
-h = Vertex("h")
-i = Vertex("i")
-j = Vertex("j")
-k = Vertex("k")
-l = Vertex("l")
-
-p = [a,b,c,d,e,f,g,h,i,j,k,l]
-
-input_edge(a, b,1)
-input_edge(e, b,1)
-input_edge(e, f,1)
-input_edge(a, c,1)
-input_edge(c, d,1)
-input_edge(d, f,1)
-input_edge(d, g,1)
-input_edge(g, h,1)
-input_edge(g, i,1)
-input_edge(i, j,1)
-input_edge(i, k,1)
-input_edge(i, l,1)
-input_edge(k, l,1)
-
+def breadth_first_traversal(graph, start, destination):
+    maze = {}
+    path = []
+    q = CircularQueue(len(graph))
+    visitedVerteces = []
+    
+    q.enqueue(start)
+    visitedVerteces.append(start)
+    while q.is_empty() == False:
+        currentVertex = q.dequeue()
+        for neighbour in currentVertex.neighbours:
+            if neighbour not in visitedVerteces:
+                maze[neighbour] = currentVertex
+                q.enqueue(neighbour)
+                visitedVerteces.append(neighbour)
+                
+    path.append(destination)
+    currentVertex = destination
+    while currentVertex != start:
+        try:
+            path.append(maze[currentVertex])
+            currentVertex = maze[currentVertex]
+        except:
+            print("No valid path.")
+            quit()
+            
+    reversedPath = reversed(path)
+    for vertex in reversedPath:
+        print(vertex.name)
+    return(reversedPath)
+          
 def sort_neighbours(vertex):
     for passes in range(len(vertex.neighbours)):
         madeSwap = False
         for comparisons in range(len(vertex.neighbours)-passes-1):
             if vertex.neighbours[comparisons].name > vertex.neighbours[comparisons+1].name:
                 madeSwap = True
-                vertex.neighbours[comparisons].name, vertex.neighbours[comparisons+1].name = vertex.neighbours[comparisons+1].name, vertex.neighbours[comparisons].name
+                vertex.neighbours[comparisons], vertex.neighbours[comparisons+1] = vertex.neighbours[comparisons+1], vertex.neighbours[comparisons]
         if madeSwap == False:
             break
 
-for vertex in p:
-    sort_neighbours(vertex)
+if __name__ == "__main__":
+    
+    a = Vertex("a")
+    b = Vertex("b")
+    c = Vertex("c")
+    d = Vertex("d")
+    e = Vertex("e")
+    f = Vertex("f")
+    g = Vertex("g")
+    h = Vertex("h")
+    i = Vertex("i")
+    j = Vertex("j")
 
-depth_first_traversal(p)
-print(a.entryTime, a.exitTime)
+    p = [a,b,c,d,e,f,g,h,i,j]
+    
+    input_edge(a,b,1)
+    input_edge(a,c,1)
+    input_edge(c,b,1)
+    input_edge(c,j,1)
+    input_edge(c,d,1)
+    input_edge(d,e,1)
+    input_edge(e,f,1)
+    input_edge(d,f,1)
+    input_edge(g,d,1)
+    input_edge(g,f,1)
+    input_edge(g,h,1)
+    input_edge(g,i,1)
+
+    for vertex in p:
+        sort_neighbours(vertex)
+
+    # depth_first_traversal(p)
+    # print(a.entryTime, a.exitTime)
+    # print(b.entryTime, b.exitTime)
+    
+    breadth_first_traversal(p, g, e)
